5ea5d212a80d6411f4d6d7749b7feb235538289c
hbh7 <newhbh7@gmail.com>
2016-02-09 20:18:28 -0500

    Image and overall design manipulation

--- imagepreload.js
+++
@@ -1,9 +0,0 @@
-var img = new Image();
-	img.src = 'backgrounds/rotate.php';
-
-var int = setInterval(function() {
-	if (img.complete) {
-		clearInterval(int);
-		document.getElementByTagName('body')[0].style.backgroundImage = 'url(' + img.src + ')';
-	}
-}, 50);
